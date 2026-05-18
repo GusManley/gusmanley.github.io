1a51a62650ea74fc8b8311445e5e1a59d2f14b04
gus manley <manleyangus17@gmail.com>
2026-05-18 20:52:56 +1000

    test for mobile fixes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
         function openNav() {
-            document.getElementById("mySidepanel").style.width = "250px";
+            document.getElementById("mySidepanel").style.width = "250svh";
             document.getElementById("overlay").style.display = "block";
         }
 
